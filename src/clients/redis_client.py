8099ed807ca425b424f5b6af85e5765fbfa52e60
Dana299 <gorevadana@gmail.com>
2024-08-28 20:31:12 +0300

    feat: add redis client implementation

--- src/clients/redis_client.py
+++ src/clients/redis_client.py
@@ -1,5 +1,39 @@
-from redis import Redis
+from typing import Optional
+
+import redis
+
+from src.exceptions import RedisConnectionError
 
 
-class RedisCLient:
-    pass
+class RedisClient:
+
+    def __init__(self, redis_dsn: str):
+        self.redis_dsn = redis_dsn
+        self.conn = None
+
+    def connect(self):
+        if not self.conn:
+            try:
+                self.conn = redis.Redis.from_url(self.redis_dsn, decode_responses=True)
+                self.conn.ping()
+            except redis.ConnectionError:
+                raise
+
+    def set_dict_with_ttl(self, key: str, mapping: dict, ttl: Optional[int] = None):
+        """Set a dict in the hash with a TTL (time to live)."""
+        if not self.conn:
+            raise RedisConnectionError("No connection with redis established")
+        self.conn.hset(key, mapping=mapping)
+        if ttl:
+            self.conn.expire(key, ttl)
+
+    def get_dict_by_key(self, key: str):
+        """Get the value in a hash stored at key."""
+        if not self.conn:
+            raise RedisConnectionError("No connection with redis established")
+        return self.conn.hgetall(key)
+
+
+if __name__ == '__main__':
+    client = RedisClient("redis://localhost:6379")
+    client.connect()
